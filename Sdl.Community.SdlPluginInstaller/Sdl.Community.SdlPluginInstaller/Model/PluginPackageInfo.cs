9df2592941789caedc2648fdeab79d18c69b007f
patrickporter <patrickport@gmail.com>
2015-07-30 14:42:54 -0400

    Use DotNetZip to extract maxversion from plugin manifest

--- Sdl.Community.SdlPluginInstaller/Sdl.Community.SdlPluginInstaller/Model/PluginPackageInfo.cs
+++ Sdl.Community.SdlPluginInstaller/Sdl.Community.SdlPluginInstaller/Model/PluginPackageInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Sdl.Core.PluginFramework.PackageSupport;
+using Ionic.Zip;
 
 namespace Sdl.Community.SdlPluginInstaller.Model
 {
@@ -17,6 +18,48 @@ namespace Sdl.Community.SdlPluginInstaller.Model
             
         }
 
+        /// <summary>
+        /// Uses DotNetZip library to return the maxversion from the manifest in the compressed sdlplugin file.
+        /// </summary>
+        /// <param name="pluginPackagePath">The path to the plugin file.</param>
+        /// <returns>The version corresponding to the maxversion, or null if none is found.</returns>
+        private static Version GetMaxVersion(string pluginPackagePath)
+        {
+            string manifestFilename = "pluginpackage.manifest.xml";
+            string contents = null;
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (ZipFile zip = new ZipFile(pluginPackagePath))
+                {
+                    foreach (ZipEntry e in zip)
+                    {
+                        if (e.FileName.Equals(manifestFilename))
+                        {
+                            e.Extract(ms);
+                            ms.Position = 0;
+                            using (StreamReader reader = new StreamReader(ms))
+                            {
+                                contents = reader.ReadToEnd();
+                            }
+                        }
+                    }
+                }
+            }
+
+            string result = null;
+            System.Xml.XmlDocument xmlDoc = new System.Xml.XmlDocument();
+            xmlDoc.LoadXml(contents);
+            try
+            {
+                result = xmlDoc["PluginPackage"]["RequiredProduct"].Attributes["maxversion"].Value;
+            }
+            catch { }
+
+            if (result == null) return null;
+            return new Version(result);
+        }
+        
         public static PluginPackageInfo CreatePluginPackageInfo(string pluginPackagePath)
         {
             var packageInfo = new PluginPackageInfo();
@@ -26,8 +69,9 @@ namespace Sdl.Community.SdlPluginInstaller.Model
                 packageInfo.Description = pluginPackage.PackageManifest.Description;
                 packageInfo.Path = pluginPackage.FilePath;
                 packageInfo.MinRequiredProductVersion = pluginPackage.PackageManifest.MinRequiredProductVersion;
-                packageInfo.MaxRequiredProductVersion = pluginPackage.PackageManifest.MaxRequiredProductVersion;
             }
+            
+            packageInfo.MaxRequiredProductVersion = GetMaxVersion(pluginPackagePath);
             return packageInfo;
         }
     }
