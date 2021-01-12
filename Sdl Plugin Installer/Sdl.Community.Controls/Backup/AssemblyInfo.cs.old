066bbd5b8eb5cd8de67202fc1e3acd4edc656559
Ionel Moldovan <imoldovan@sdl.com>
2021-01-12 11:58:49 +0200

    convert csproj to sdk

---
+++ Sdl Plugin Installer/Sdl.Community.Controls/Backup/AssemblyInfo.cs.old
@@ -0,0 +1,14 @@
+﻿#region References
+using System.Reflection;
+using System.Runtime.CompilerServices;
+#endregion
+
+[assembly: AssemblyTitle("Sdl.Community.Controls")]
+[assembly: AssemblyDescription("A Custom Controls Library for .NET")]
+
+
+[assembly: AssemblyDelaySign(false)]
+[assembly: AssemblyKeyFile("")]
+[assembly: AssemblyKeyName("")]
+
+
---
+++ Sdl Plugin Installer/Sdl.Community.SdlPluginInstaller/Sdl.TranslationStudio.Info.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyVersion("16.0.0.0")]
+[assembly: AssemblyInformationalVersion("Studio16")]
+[assembly: AssemblyFileVersion("16.0.0.0")]
+[assembly: AssemblyCopyright("Copyright © 2020 SDL. All rights reserved.")]
+[assembly: AssemblyCompany("SDL")]
+[assembly: AssemblyProduct("SDL Trados Studio")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
